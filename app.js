47cfac46e203ce7f2485abccf40da17ddbe5c3db
Brian Bycynski <high5bri@hotmail.com>
2021-12-11 02:23:46 -0500

    colors project

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll('.color h2');
 
 const popup = document.querySelector('.copy-container');
+const adjustButton = document.querySelectorAll('.adjust');
+
+const closeAdjustment = document.querySelectorAll('.close-adjustment');
+const sliderContainers = document.querySelectorAll('.sliders');
 
 let initialColors;
 
@@ -33,6 +37,18 @@ popup.addEventListener('transitionend', () => {
   const popupBox = popup.children[0];
   popup.classList.remove('active');
   popupBox.classList.remove('active');
+});
+
+adjustButton.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    openAdjustmentPanel(index);
+  });
+});
+
+closeAdjustment.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    closeAdjustmentPanel(index);
+  });
 });
 
 // Functions
@@ -180,6 +196,14 @@ function copyToClipboard(hex) {
   const popupBox = popup.children[0];
   popup.classList.add('active');
   popupBox.classList.add('active');
+}
+
+function openAdjustmentPanel(index) {
+  sliderContainers[index].classList.toggle('active');
+}
+
+function closeAdjustmentPanel(index) {
+  sliderContainers[index].classList.remove('active');
 }
 
 randomColors();
