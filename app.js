44603b5223e5117086bcfd5cdbef5fe470b2e9a9
Brian Bycynski <high5bri@hotmail.com>
2021-12-12 03:37:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ const currentHexes = document.querySelectorAll('.color h2');
 const popup = document.querySelector('.copy-container');
 const adjustButton = document.querySelectorAll('.adjust');
 
+const lockButton = document.querySelectorAll('.lock');
+
 const closeAdjustment = document.querySelectorAll('.close-adjustment');
 const sliderContainers = document.querySelectorAll('.sliders');
 
 let initialColors;
 
 // Event Listeners
+
+generateBtn.addEventListener('click', randomColors);
+
 sliders.forEach((slider) => {
   slider.addEventListener('input', hslControls);
 });
@@ -86,6 +91,12 @@ function randomColors() {
   });
   // Reset Inputs
   resetInputs();
+
+  // Check for Button contrast
+  adjustButton.forEach((button, index) => {
+    checkTextContrast(initialColors[index], button);
+    checkTextContrast(initialColors[index], lockButton[index]);
+  });
 }
 
 function checkTextContrast(color, text) {
